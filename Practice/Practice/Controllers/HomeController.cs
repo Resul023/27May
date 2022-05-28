3bb32bfd1b1e834c6d67bf7c912dfc8fde2d7608
Resul023 <73m6u7h@code.edu.az>
2022-05-28 02:49:49 -0700

    first commit

---
+++ Practice/Practice/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ContentResult Text()
+        {
+            var result = Content("Salamlar");
+            return result;
+        }
+        public ViewResult Index() 
+        {
+            return View();
+        }
+
+        public ActionResult ShowId(int id) 
+        {
+            TempData["showId"] = id;
+            ViewData["showId"] = id;
+            ViewBag.showId = id;
+            return View();
+        }
+    }
+}
